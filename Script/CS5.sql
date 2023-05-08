4a67d6622d91a19cdfe26ab883188fe1544da2aa
Nguyễn Trung Nguyên <20127404@student.hcmus.edu.vn>
2023-05-09 00:03:28 +0700

    submission

--- Script/CS5.sql
+++ Script/CS5.sql
@@ -1,8 +1,19 @@
 --cs5--
+
+grant GeneralRole to NhanSu
+grant NhanVien to NhanSu
+grant update(ngaysinh,diachi,sodt) on NhanVien  to NhanSu;
+grant select on dean to Nhansu
+grant select on Phongban to Nhansu;
 grant update on Phongban to Nhansu;
 grant insert on phongban to Nhansu;
-CREATE OR REPLACE VIEW NhanVien_Select_NS AS
-SELECT distinct NV.MANV,NV.Username,NV.TENNV,NV.PHAI,NV.NGAYSINH,NV.DIACHI,NV.SODT,NV.VAITRO,NV.MANQL,NV.PHG,decode(NV.Username,SYS_CONTEXT ('USERENV', 'SESSION_USER'),luong) luong,decode(NV.Username,SYS_CONTEXT ('USERENV', 'SESSION_USER'),phucap) phucap
+create or replace view NhanSu_EditProfile as --- View nay de nhan su chinh sua thong tin ca nhan
+select NV.MANV,NV.TENNV,NV.NGAYSINH,NV.DIACHI,NV.SODT,CAST (decrypt_luong(luong) AS varchar2 (255)) as luong, CAST (decrypt_phucap(phucap) AS varchar2 (255)) as phucap from nhanvien NV
+where Username = SYS_CONTEXT ('USERENV', 'SESSION_USER')
+with check option;
+grant select on NhanSu_EditProfile to Nhansu;
+CREATE OR REPLACE VIEW NhanVien_Select_NS AS --- View nay de nhan su xem tat ca cac thong tin cua NV khac tru LUONG VA PHUCAP
+SELECT distinct NV.MANV,NV.Username,NV.TENNV,NV.PHAI,NV.NGAYSINH,NV.DIACHI,NV.SODT,NV.VAITRO,NV.MANQL,NV.PHG,decode(NV.Username,SYS_CONTEXT ('USERENV', 'SESSION_USER'),CAST (decrypt_luong(luong) AS varchar2 (255))) luong,decode(NV.Username,SYS_CONTEXT ('USERENV', 'SESSION_USER'),CAST (decrypt_luong(phucap) AS varchar2 (255))) phucap
 FROM NHANVIEN NV
 union
 SELECT distinct NV2.MANV,NV2.Username,NV2.TENNV,NV2.PHAI,NV2.NGAYSINH,NV2.DIACHI,NV2.SODT,NV2.VAITRO,NV2.MANQL,NV2.PHG,decode(NV2.Username,null,NV2.luong) luong,decode(NV2.Username,null,Nv2.phucap) phucap
@@ -25,5 +36,5 @@ END;
 /
 
 GRANT EXECUTE ON update_NV_NS TO NhanSu;
-grant Nhansu to NS;
-grant GeneralRole to NS;
+--grant Nhansu to NS;
+--grant GeneralRole to NS;
